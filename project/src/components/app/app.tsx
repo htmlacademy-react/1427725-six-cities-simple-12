406bbd6ed703dbff35c6bce788af94518a1a225b
Matvey Nekrasov <metal59@gmail.com>
2023-03-13 18:26:56 +0700

    Подключены моки в index.tsx

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,12 +4,16 @@ import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import RoomScreen from '../../pages/room-screen/room-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import { Offers } from '../../types/offer';
+import { Reviews } from '../../types/review';
 
 type AppProps = {
   cardsCount: number;
+  offers: Offers;
+  reviews: Reviews;
 }
 
-function App({ cardsCount }: AppProps): JSX.Element {
+function App({ cardsCount, offers, reviews }: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { offers } from './mocks/offers';
+import { reviews } from './mocks/reviews';
 
 const Settings = {
   CardsCount: 3,
@@ -14,6 +16,8 @@ root.render(
   <React.StrictMode>
     <App
       cardsCount={Settings.CardsCount}
+      offers={offers}
+      reviews={reviews}
     />
   </React.StrictMode>,
 );
