b40caf3f66d5dfc9676c5d4cfb815a62fde1c9b3
Matvey Nekrasov W <metal59@gmail.com>
2023-03-13 20:54:56 +0000

    Исправлено округление оценки в карточке

--- project/src/components/card/card.tsx
+++ project/src/components/card/card.tsx
@@ -38,7 +38,7 @@ function Card({ offer }: CardProps): JSX.Element {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: `${Math.round(rating * 20)}%` }}></span>
+            <span style={{ width: `${Math.round(rating) * 20}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
