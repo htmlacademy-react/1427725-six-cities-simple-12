431c182e9746c8b6666b1857522436aeff254470
Matvey Nekrasov <metal59@gmail.com>
2023-03-19 18:21:21 +0700

    Добавлены маркеры, useMap

--- project/src/const.ts
+++ project/src/const.ts
@@ -6,3 +6,9 @@ export enum AppRoute {
 }
 
 export const REVIEW_MIN_LENGTH = 50;
+
+export const URL_MARKER_DEFAULT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+
+export const URL_MARKER_CURRENT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
