9f5e8525398c81d5fa3fc8c62e33e235a0d38791
Matvey Nekrasov <metal59@gmail.com>
2023-03-26 12:56:08 +0700

    Добавлено отрытие/скрытие сортировки

--- project/src/components/sorting/sorting.tsx
+++ project/src/components/sorting/sorting.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
+import cn from 'classnames';
+
 function Sorting(): JSX.Element {
+  const [isOpened, setIsOpened] = useState(false);
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => { setIsOpened(!isOpened); }}>
         Popular
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul className={cn('places__options', 'places__options--custom', { 'places__options--opened': isOpened })}>
         <li className="places__option places__option--active" tabIndex={0}>Popular</li>
         <li className="places__option" tabIndex={0}>Price: low to high</li>
         <li className="places__option" tabIndex={0}>Price: high to low</li>
