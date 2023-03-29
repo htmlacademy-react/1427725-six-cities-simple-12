344566a087fcdf23ff1fdb2464df04e37d3c3762
Matvey Nekrasov <metal59@gmail.com>
2023-03-29 20:00:57 +0700

    Создан компонент для отображения ошибки и подключен

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
+import ErrorMessage from './components/error-message/error-message';
 import { reviews } from './mocks/reviews';
 import { store } from './store';
 
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ErrorMessage />
       <App
         reviews={reviews}
       />
